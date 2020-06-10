417c6cd0dcae4ccc54779ec12ca700719a8b5285
janoweb <janoweb@users.noreply.github.com>
2020-06-10 19:14:19 +0100

    Post method

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 //reading JSON file from dev
 const books = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -16,6 +18,22 @@ app.get('/api/v1/books', (req, res) => {
             books
         }
     })
+});
+
+app.post('/api/v1/books', (req, res) => {
+    const newId = books[books.length - 1].id +1;
+    const newBook = Object.assign({id: newId}, req.body);
+
+    books.push(newBook);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(books), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                book: newBook
+            }
+        });
+    }); 
 });
 
 
