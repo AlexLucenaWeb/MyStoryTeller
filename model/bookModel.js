42699bc9c4e84e20d10175fb4e00439349918c15
janoweb <janoweb@users.noreply.github.com>
2020-06-13 17:13:08 +0100

    book model

--- model/bookModel.js
+++ model/bookModel.js
@@ -3,17 +3,59 @@ const mongoose = require('mongoose');
 const bookSchema =new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'The book must have a name'],
-        unique: true
+        required: [true, 'A book must have a name'],
+        unique: true,
+        trim: true,
+        maxlength: [40, 'A book name must have less or equal then 40 characters'],
+        minlength: [10, 'A book name must have more or equal then 10 characters']
     },
-    pages: {
-        type: Number,
-        default: 7
+    category:{
+        type: String,
+        required: [true, 'A book must have a category'],
+        enum: {
+            values: ['nature', 'home', 'family', 'friends', 'holidais', 'school', 'music', 'animals'],
+            message: 'Category is either: nature, home, family, friends, holidais, school, music or animals'
+          }
     },
     age: {
         type: String,
-        required: [true, 'The book must have a recomended age']
-    }
+        required: [true, 'The book must have a recomended age'],
+        trim: true
+    },
+    free: { 
+        type: boolean,
+        default: false
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0']  
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    music: [String],
+    voice: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+},
+{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 const Book = mongoose.model('Book', bookSchema);
 
