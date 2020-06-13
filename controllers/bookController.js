d7ed743663d926be375a4b9ea4a1472b4e57fef3
janoweb <janoweb@users.noreply.github.com>
2020-06-13 16:28:19 +0100

    delete and update

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -54,17 +54,36 @@ exports.createBook = async (req, res) => {
     }
 }
 
-exports.deleteBook = (req, res) => {   
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
+exports.updateBook = async (req, res) => {  
+    try{
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                book
+            }   
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }  
 }
-exports.updateBook = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            book:'<Updated>' 
-        }
-    });
+exports.deleteBook = async (req, res) => {
+    try{
+        await Book.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: 'success',
+            data: null  
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }  
 }
