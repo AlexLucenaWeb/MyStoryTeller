fe07c904a2c7f3e91d839a80cb04ad5279da5b28
janoweb <janoweb@users.noreply.github.com>
2020-06-17 19:50:15 +0100

    advance error controller prod or dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,43 @@
 // Error handling middleware:
+const sendErrorDev = (err, res) =>{
+    res.status(err.statusCode).json({ 
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) => {
+
+    // Sending the error to the client if it is Operational
+    if(err.isOperational){
+        res.status(err.statusCode).json({ 
+            status: err.status,
+            message: err.message,
+        });
+
+    // Not sending unknown error.
+    } else {
+        // 1- Log the error
+        console.error('ERROR!!!!', err);
+        // 2- Send a generic error
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong :('
+        });
+    };
+};
+
 module.exports = (err, req, res, next) => {
     // console.log(err.stack);
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
     
-    res.status(err.statusCode).json({ 
-        status: err.status,
-        message: err.message
-    });
+    if (process.env.NODE_ENV === 'development'){
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production'){
+        sendErrorProd(err, res);
+    }
 };
