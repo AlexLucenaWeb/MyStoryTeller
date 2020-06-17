254d51e5afa66b89602391c24d720bc1a0709267
janoweb <janoweb@users.noreply.github.com>
2020-06-17 13:08:36 +0100

    not find route handler

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const bookRouter = require('./routes/bookRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
 // 1- Middleware functions
 if(process.env.NODE_ENV === 'development '){
    app.use(morgan('dev'));
 }
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-    console.log('Hello from middleware');
-    next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -25,5 +22,14 @@ app.use((req, res, next) => {
 // 3- Routes:   
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/users', userRouter); 
+
+// Route handler for not implemented routes:
+// Using all for all methods and * for all routes.
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can´t find the route ${req.originalUrl} on this server :(`
+    });
+});
 
 module.exports = app;
