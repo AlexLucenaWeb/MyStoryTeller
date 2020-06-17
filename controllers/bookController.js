41b590f8b8ae8eb259bb4f67bd5a0456a0e6812e
janoweb <janoweb@users.noreply.github.com>
2020-06-17 19:36:42 +0100

    adding 404 error handler for no ID in all controler needed

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,7 @@
 const Book = require('./../model/bookModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 // TOP BOOKS
 exports.topBook = async (req, res, next) => {
@@ -11,44 +12,55 @@ exports.topBook = async (req, res, next) => {
 };
 
 // GET ALL BOOKS AND FILTERING.
-exports.getAllBooks = catchAsync (async (req, res, next) => {
-     // EXECUTE THE QUERY:
-    // Adding features from class APIFeatures:
-    const features = new APIFeatures(Book.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const books = await features.query;
+exports.getAllBooks = catchAsync(async (req, res, next) => {
+  // EXECUTE THE QUERY:
+  // Adding features from class APIFeatures:
+  const features = new APIFeatures(Book.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const books = await features.query;
 
-    //SEND THE QUERY:
-    res.status(200).json({
-      status: 'success',
-      result: books.length,
-      data: {
-        books,
-      },
-    });
+  //SEND THE QUERY:
+  res.status(200).json({
+    status: 'success',
+    result: books.length,
+    data: {
+      books,
+    },
+  });
 });
 
 // GET ONE BOOK
-exports.getBook = catchAsync (async (req, res, next) => {
+exports.getBook = catchAsync(async (req, res, next) => {
   const book = await Book.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        book,
-      },
-    });
+  // Handling invalid ID 404 error
+  if(!book){
+    return next(new AppError('No book found with this ID', 404))
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      book,
+    },
+  });
 });
 
 // UPDATE A BOOK
-exports.updateBook = catchAsync (async (req, res, next) => {
+exports.updateBook = catchAsync(async (req, res, next) => {
   const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
+
+  // Handling invalid ID 404 error
+  if(!book){
+    return next(new AppError('No book found with this ID', 404))
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -59,11 +71,17 @@ exports.updateBook = catchAsync (async (req, res, next) => {
 
 // DELETE A BOOK
 exports.deleteBook = catchAsync(async (req, res, next) => {
-  await Book.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
+  const book = await Book.findByIdAndDelete(req.params.id);
+
+  // Handling invalid ID 404 error
+  if(!book){
+    return next(new AppError('No book found with this ID', 404))
+  }
+  
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 // CREATE A BOOK
