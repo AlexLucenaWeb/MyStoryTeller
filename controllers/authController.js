fde5bb4532d0ea7f976dd7576b2355a95d978e7c
janoweb <janoweb@users.noreply.github.com>
2020-06-18 20:10:12 +0100

    instaling  and using jwt to create tokens for the user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,24 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
 // const AppError = require('./../utils/appError');
 
 exports.singup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password, 
+        passwordConfirmation: req.body.passwordConfirmation,
+    });
+
+    // TOKEN, using json web token:
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });    
 
     res.status(201).json({
         status: 'success',
+        token,
         data:{
             user: newUser
         }
