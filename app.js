3283ad5d5de4fd9175540bc20e951b2b665ac811
janoweb <janoweb@users.noreply.github.com>
2020-06-21 21:35:46 +0100

    rating limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1- Middleware functions
+//  -----===== GLOBAL MIDDLEWARE FUNCTIONS  =====-----
+
+// --  Using morgan in development  ---
 if(process.env.NODE_ENV === 'development '){
    app.use(morgan('dev'));
 }
+
+// -- Limiting requests  ---
+const limiter = rateLimit({
+    max: 300,
+    windowMs: 60 * 60 * 1000,
+    message: 'To many requests from this IP, please try again in an hour.'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -19,15 +30,14 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     // console.log(req.headers);
-
     next();
 });
 
-// 3- Routes:   
+// -----=====  ROUTES  =====-----   
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/users', userRouter); 
 
-// Route handler for not implemented routes:
+//  --  Route handler for not implemented routes  --
 // Using all for all methods and * for all routes.
 app.all('*', (req, res, next) => {
     // passing and argument in next, express asumme it is an error and skip all steps and send it global error handlr
