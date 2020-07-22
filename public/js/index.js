f9976cd897429c45f0f8a30c9ac6125c7741cc37
janoweb <janoweb@users.noreply.github.com>
2020-07-22 17:39:40 +0100

    finaly omg update data

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,20 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 // -----===== DOM ELEMENTS   =====-----
 const loginForm = document.querySelector('.login-container');
 const logOutBtn = document.querySelector('.nav__logoutBtn');
+const userDetailsForm = document.querySelector('.form-acc-details');
+// const prueba = document.querySelector('.user__menu');
+// const prueba = $('#prueba');
 
 // -----===== DELEGATES =====-----
+
+// if(prueba){
+//     prueba.style.background = 'red';
+// }
+
 if (loginForm){
     loginForm.addEventListener('submit', e => {
         e.preventDefault();
@@ -16,3 +25,12 @@ if (loginForm){
 }
 
 if(logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDetailsForm){
+    userDetailsForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        updateSettings(name, email);
+    });
+}
