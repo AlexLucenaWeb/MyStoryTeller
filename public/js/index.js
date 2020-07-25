6961bb2cc80065dd0f7c7b70b37f8f8f8c2c29ed
janoweb <janoweb@users.noreply.github.com>
2020-07-25 21:43:32 +0100

    building fornt end, styles, full heigth pages and login bug fixed

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,9 @@ import { updateSettings, deleteMeAcc } from './updateSettings';
 
 // -----===== DOM ELEMENTS   =====-----
 const signupForm = document.querySelector('#signup__form');
-const loginForm = document.querySelector('.login-container');
-const logOutBtn = document.querySelector('.nav__logoutBtn');
+const loginForm = document.querySelector('#login__form');
+const logOutBtn = document.querySelector('#logout__btn');
+const logOutBtnTwo = document.querySelector('#logout__btnTwo');
 const userDetailsForm = document.querySelector('.form-acc-details');
 const userPasswordForm = document.querySelector('.form-acc-password');
 const deleteAccBtn = document.querySelector('#deleteAcc');
@@ -38,7 +39,9 @@ if (loginForm)
 
 // -- logout  --
 
-if(logOutBtn) logOutBtn.addEventListener('click', logout);
+if(logOutBtn || logoutBtnTwos) 
+    logOutBtn.addEventListener('click', logout);
+    logOutBtnTwo.addEventListener('click', logout);
 
 // -- Update user profile  --
 
@@ -80,7 +83,7 @@ if(deleteAccBtn)
 //  -----=====  ANIMATIONS AND VISUAL EFFECTS  =====-----
 
 //  --  Index page  --
-$('body').mousemove(function(e){
+$('.index').mousemove(function(e){
     let moveX1 = (e.pageX * -1 /60);
     let moveY1 = (e.pageY * -1 /60);
     let moveX2 = (e.pageX * -1 /75);
@@ -94,6 +97,4 @@ $('body').mousemove(function(e){
     $('#tree-2').css('background-position', moveX2 + 'px ' + moveY2 + 'px ');
     $('#tree-3').css('background-position', moveX3 + 'px ' + moveY3 + 'px ');
     $('#tree-4, #sun-1, #sun-2').css('background-position', moveX4 + 'px ' + moveY4 + 'px ');
-    // $('#sun-1').css('background-position', moveX4 + 'px ' + moveY4 + 'px ');
-    // $('#sun-2').css('background-position', moveX4 + 'px ' + moveY4 + 'px ');
 });
